6cd38158f52e80ac531d47076a0064e5527dad14
FFFluoride <evandrofilipe108@protonmail.com>
2025-03-24 15:28:58 +0000

    Got my act together in terms of code tangling in org mode. Now the script is pretty convenient to use. Better start curating my playlists.

--- commands.bash
+++ commands.bash
@@ -1,3 +1,7 @@
+# Playlists
+
+# This is how I associate youtube's playlist ID's to easily rememberable playlist file names.
+
 playlists=(
     PLEITERw4PaKFs_uS0MoXkIfGbsYJHmWVU:balatro.m3u
     PLY9u9wC7ApRJk-tG4_pBtH6ubEJ7svnOB:celeste.m3u
@@ -19,6 +23,13 @@ playlists=(
     PLHVUN_wezMf9LHGahDuuIiPlog4LI9J8k:rhythm_doctor.m3u
 )
 
+# Generating the playlist files concurrently
+
+# This is pretty gnarly by my standards.
+# Basically, you make an array to sequence commands by looping once and then running the long running tasks in parallel by launching multiple subshells and using '&'.
+
+# It's important that this runs in a subshell or else ctrl-c wouldn't kill all the processes.
+
 commands=()
 
 mkdir playlists
@@ -29,7 +40,19 @@ done
 
 (trap 'kill 0' SIGINT; eval ${commands[*]} wait)
 
+# Generating custom playlists
+
+# This simply generates the everything playlist which represents all the songs.
+
+# More playlists coming soon...
+
 cat celeste.m3u hollow_knight.m3u terraria.m3u calamity_infernum.m3u baba.m3u deltarune.m3u undertale.m3u isaac_rebirth.m3u isaac_afterbirth.m3u isaac_repentance.m3u isaac_antibirth.m3u minecraft.m3u stardew_valley.m3u cassette_beasts.m3u RoA.m3u rhythm_doctor.m3u > playlist/everything.m3u
+
+# Slowing down balatro tracks
+
+# I wanted to sequence this, however, I couldn't get this to work for whatever reason, it seemed to hang. It's only 5 tracks though, so it's not really a big issue.
+
+# Tracks are slowed down to 75% of of their original speed, just like in the game.
 
 handle_balatro () {
     mkdir -p "pool/tmp"
@@ -46,6 +69,10 @@ handle_balatro () {
     rm "pool/tmp" -r
 }
 
+# Downloading all the playlist songs in parallel
+
+# Again, I sequence operations, then run them all at once.
+
 commands=()
 
 for playlist_item in ${playlists[@]}; do
@@ -57,20 +84,3 @@ for playlist_item in ${playlists[@]}; do
 done;
 
 (trap 'kill 0' SIGINT; eval ${commands[*]} wait)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
